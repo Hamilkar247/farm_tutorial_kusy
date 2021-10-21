7fd5725560bbe9ac6eb25da52da7a84317000b06
Hamilkar247 <adonnibal96@gmail.com>
2021-10-21 18:10:59 +0200

    [WO-7] finiszowanie, acz nadal z błędami
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,29 @@ import './App.css';
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import TodoView from './components/TodoListView';
 
 function App() {
+
+  const [todoList, setTodoList] = useState([{}])
+  const [title, setTitle] = useState('')
+  const [desc, setDesc] = useState('')
+
+  // Read all todos
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/todo')
+      .then(res => {
+        setTodoList(res.data)
+      })
+  });
+
+  const addTodoHandler = () => {
+    axios.post('http://localhost:8000/api/todo/', { 'title':
+    title, 'description': desc })
+    .then(res => console.log(res))
+  };
+
   return (
-
-
       <div className="App list-group-item justify-content-center align-items-center mx auto" style={{"width": "400px", "backgroundColor": "white", "marginTop": "15px"}}>
             Hello, world!
       FARM STACK
@@ -16,13 +34,13 @@ function App() {
         <div className="card-body">
           <h5 className="card text-white bg-dark mb-3">Add your task</h5>
           <span className="card-text">
-            <input className="mb-2 form-control titleIn" placeholder='Title'/>
-            <input className="mb-2 form-control desIn" placeholder='Description'/>
-            <button className="btn btn-outline-primary mx-2 mb-2" style={{'borderRadius':'50px', "font-weight":"bold"}}>Add Task</button>
+            <input className="mb-2 form-control titleIn" onChange={event => setTitle(event.target.value)} placeholder='Title'/>
+            <input className="mb-2 form-control desIn" onChange={event => setDesc(event.target.value)} placeholder='Description'/>
+            <button className="btn btn-outline-primary mx-2 mb-3" style={{'borderRadius':'50px', "font-weight":"bold"}} onClick={addTodoHandler}>Add Task</button>
           </span>
           <h5 className="card text-white bg-dark mb-3">Yours Tasks</h5>
           <div>
-            {/* Todo items - external component */}
+          <TodoView todoList= {todoList} />
           </div>
         </div>
               <h6 className="card text-dark bg-warning py-1 mb-0" >Copyright 2021, All rights reserved &copy;</h6>
